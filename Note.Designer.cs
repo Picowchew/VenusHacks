735b6489eb83e1ab273a23112cc644546a26a927
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-04-24 18:00:18 -0400

    Moved files out of folder

---
+++ Note.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Venus_Hackathons_project
+{
+    partial class Note
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1n = new System.Windows.Forms.Label();
+            this.label2n = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1n
+            // 
+            this.label1n.AutoSize = true;
+            this.label1n.Location = new System.Drawing.Point(100, 146);
+            this.label1n.Name = "label1n";
+            this.label1n.Size = new System.Drawing.Size(50, 13);
+            this.label1n.TabIndex = 0;
+            this.label1n.Text = "Message";
+            // 
+            // label2n
+            // 
+            this.label2n.AutoSize = true;
+            this.label2n.Location = new System.Drawing.Point(100, 67);
+            this.label2n.Name = "label2n";
+            this.label2n.Size = new System.Drawing.Size(43, 13);
+            this.label2n.TabIndex = 1;
+            this.label2n.Text = "Subject";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(406, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 139);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(406, 103);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Note
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2n);
+            this.Controls.Add(this.label1n);
+            this.Name = "Note";
+            this.Text = "Note";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1n;
+        private System.Windows.Forms.Label label2n;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Tip.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Venus_Hackathons_project
+{
+    partial class Tip
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1t = new System.Windows.Forms.Label();
+            this.label2t = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2t = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1t
+            // 
+            this.label1t.AutoSize = true;
+            this.label1t.Location = new System.Drawing.Point(72, 61);
+            this.label1t.Name = "label1t";
+            this.label1t.Size = new System.Drawing.Size(43, 13);
+            this.label1t.TabIndex = 0;
+            this.label1t.Text = "Subject";
+            // 
+            // label2t
+            // 
+            this.label2t.AutoSize = true;
+            this.label2t.Location = new System.Drawing.Point(72, 156);
+            this.label2t.Name = "label2t";
+            this.label2t.Size = new System.Drawing.Size(50, 13);
+            this.label2t.TabIndex = 1;
+            this.label2t.Text = "Message";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(453, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2t
+            // 
+            this.textBox2t.Location = new System.Drawing.Point(173, 153);
+            this.textBox2t.Multiline = true;
+            this.textBox2t.Name = "textBox2t";
+            this.textBox2t.Size = new System.Drawing.Size(453, 118);
+            this.textBox2t.TabIndex = 3;
+            // 
+            // Tip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2t);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2t);
+            this.Controls.Add(this.label1t);
+            this.Name = "Tip";
+            this.Text = "Tip";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1t;
+        private System.Windows.Forms.Label label2t;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2t;
+    }
+}
